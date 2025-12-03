5668c9d8074e34492affe58e5aa0b6d6ab6220a4
karel375 <kar.kubin@gmail.com>
2025-12-03 14:45:47 +0100

    Update main.c

--- Buttons/src/main.c
+++ Buttons/src/main.c
@@ -7,12 +7,14 @@
 //#include <Si4703.h>
 #include "timer.h"
 
-
+// =============================================================================== SEKCE TLACITKA
 volatile uint8_t oldD;
 uint32_t actFrequency; // Aktuální hodnota frekvence >>>>>>jaká velikost?<<<<<<<<<
 uint8_t buttonPD2isPressed = 0; //Je zmáčknuté tlačítko na pinu PD2
-uint8_t buttonPD2pressedLong = 0; // tlačítko už je zmáčknuté určitou dobu na pinu PD2 >> 1
-uint8_t buttonPD2pressedLong2 = 0; // tlačítko už je zmáčknuté určitou delší dobu na pinu PD2
+uint8_t buttonPD3isPressed = 0; //Je zmáčknuté tlačítko na pinu PD3
+uint8_t buttonPD4isPressed = 0; //Je zmáčknuté tlačítko na pinu PD4
+uint8_t buttonPressedLong = 0; // tlačítko už je zmáčknuté určitou dobu >> 1
+uint8_t buttonPressedLong2 = 0; // tlačítko už je zmáčknuté určitou delší dobu >> 1
 
 
 
@@ -45,7 +47,7 @@ int main(void)
 
     while (1) {
         // hlavní smyčka prázdná
-        if (buttonPD2isPressed == 1) {
+        if (buttonPD2isPressed == 1 || buttonPD3isPressed == 1) {
           tim1_ovf_33ms();
           tim1_ovf_enable();
         } else{
@@ -58,6 +60,7 @@ int main(void)
 
 ISR(PCINT2_vect)
 {
+    
     uint8_t newD = PIND;   // čteme celý port D
 
     // PD2 (PCINT18)
@@ -69,14 +72,16 @@ ISR(PCINT2_vect)
         actFrequency = actFrequency + 100;
         SI4703_SetFreq(actFrequency);
         */
+       if (buttonPD3isPressed != 1) {
        buttonPD2isPressed = 1;
+       }
     }
 
     if ((newD & (1 << PD2)) != 0 && (oldD & (1 << PD2)) == 0) {
   
      buttonPD2isPressed = 0;
-     buttonPD2pressedLong = 0;
-     buttonPD2pressedLong2 = 0;
+     buttonPressedLong = 0;
+     buttonPressedLong2 = 0;
 
     }
 
@@ -84,56 +89,76 @@ ISR(PCINT2_vect)
     if ((newD & (1 << PD3)) == 0 && (oldD & (1 << PD3)) != 0) {
         // sem dej akci pro tlačítko PD3
         gpio_write_high(&PORTB, PB5);
+        if (buttonPD2isPressed != 1) {
+            buttonPD3isPressed = 1;
+        }
     }
+
+    if ((newD & (1 << PD3)) != 0 && (oldD & (1 << PD3)) == 0) {
+        // sem dej akci pro tlačítko PD3
+        buttonPD3isPressed = 0;
+        buttonPressedLong = 0;
+        buttonPressedLong2 = 0;
+    }
+
 
     // PD4 (PCINT20)
     if ((newD & (1 << PD4)) == 0 && (oldD & (1 << PD4)) != 0) {
         // sem dej akci pro tlačítko PD4
-        gpio_write_low(&PORTB, PB5);
+        gpio_write_low(&PORTB, PB5); // ===================================== AKCE PRO PD4 TLACITKO (SEEK)
+        // SI4703_SeekUp();   
     }
   oldD = newD;
 }
 
+volatile uint8_t initTime = 0; // pocitadlo nasobku doby preteceni timeru
+volatile uint8_t fastTime = 0; // pocitadlo pro zvyseni prodlevy pred zrychlenim
+
+
 ISR(TIMER1_OVF_vect)
 {
-    volatile initTime = 0; // cas (pocet opakovani) pro dlouhy stisk
-    volatile fastTime = 0; // rychlost zmeny frekvence pomalejsi
-
+    
     
 
-    if (buttonPD2pressedLong == 0 && buttonPD2pressedLong2 == 0) {
-        if (initTime > 30) {
-          buttonPD2pressedLong = 1;
-          gpio_toggle(&PORTB, PB5);
+    if (buttonPressedLong == 0 && buttonPressedLong2 == 0) {
+        if (initTime > 20) {
+          buttonPressedLong = 1;
+          gpio_toggle(&PORTB, PB5); // ===================================== AKCE 1 PRO PD2 A PD3 TLACITKA SPOLU (ZATIM - UP, DOWN)
           initTime = 0;
         } else {
             initTime++;
         }
-    } else if (buttonPD2pressedLong == 1 && buttonPD2pressedLong2 == 0){
+    } else if (buttonPressedLong == 1 && buttonPressedLong2 == 0){
       
-        if (initTime > 15) {
-          gpio_toggle(&PORTB, PB5);
+        if (initTime > 6) {
+          gpio_toggle(&PORTB, PB5); // ===================================== AKCE 2 PRO PD2 A PD3 TLACITKA SPOLU (ZATIM - UP, DOWN)
           initTime = 0;
           if (fastTime > 6) {
-              buttonPD2pressedLong2 = 1;
+              buttonPressedLong2 = 1;
+              fastTime = 0;
           } else {
               fastTime++;
           }
         } else {
             initTime++;
         }
-    } else if (buttonPD2pressedLong == 1 && buttonPD2pressedLong2 == 1){
+    } else if (buttonPressedLong == 1 && buttonPressedLong2 == 1){
       
-        if (initTime > 7) {
-          gpio_toggle(&PORTB, PB5);
+        if (initTime > 2) {
+          gpio_toggle(&PORTB, PB5); // ===================================== AKCE 3 PRO PD2 A PD3 TLACITKA SPOLU (ZATIM - UP, DOWN)
           initTime = 0;
         } else {
             initTime++;
         }
+    } else {
+        buttonPressedLong = 0;
+        buttonPressedLong2 = 0;
+        initTime = 0;
+        fastTime = 0;
     }
 }
 
-
+// =============================================================================== KONEC SEKCE TLACITKA
 
 
 
