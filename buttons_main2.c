8e29192f7f726a087fd647d9e90ec70a7dcddaf8
karel375 <kar.kubin@gmail.com>
2025-12-05 18:48:44 +0100

    Update buttons_main2.c

--- buttons_main2.c
+++ buttons_main2.c
@@ -4,17 +4,18 @@
 #include <string.h>
 #include <avr/interrupt.h>
 #include <gpio.h>
-//#include <Si4703.h>
+#include <Si4703.h>
 #include "timer.h"
 
-
+// =============================================================================== SEKCE TLACITKA
 volatile uint8_t oldD;
 uint32_t actFrequency; // Aktuální hodnota frekvence >>>>>>jaká velikost?<<<<<<<<<
 uint8_t buttonPD2isPressed = 0; //Je zmáčknuté tlačítko na pinu PD2
 uint8_t buttonPD3isPressed = 0; //Je zmáčknuté tlačítko na pinu PD3
 uint8_t buttonPD4isPressed = 0; //Je zmáčknuté tlačítko na pinu PD4
-uint8_t buttonPD2pressedLong = 0; // tlačítko už je zmáčknuté určitou dobu na pinu PD2 >> 1
-uint8_t buttonPD2pressedLong2 = 0; // tlačítko už je zmáčknuté určitou delší dobu na pinu PD2
+uint8_t buttonPressedLong = 0; // tlačítko už je zmáčknuté určitou dobu >> 1
+uint8_t buttonPressedLong2 = 0; // tlačítko už je zmáčknuté určitou delší dobu >> 1
+uint8_t zkouska12 = 1; // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx potom smazat
 
 
 
@@ -25,18 +26,15 @@ int main(void)
     gpio_mode_input_pullup(&DDRD, PD3);
     gpio_mode_input_pullup(&DDRD, PD4);
 
+    if (zkouska12 == 1){ // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx vvvvvvv
     // --- Výstup PB0 (LED) ---
     gpio_mode_output(&DDRB, PB5);
+    } // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ^^^^^^^
 
     oldD = PIND;   // uložit počáteční stav portu D
-    
 
     // --- Povolit PCINT2 skupinu ---
     PCICR |= (1 << PCIE2);
-
-    
-    
-    
 
     // Povolit PCINT18, PCINT19, PCINT20 (PD2, PD3, PD4)
     PCMSK2  |= (1 << PCINT18)     // PD2
@@ -46,52 +44,86 @@ int main(void)
     sei(); // globální povolení přerušení
 
     while (1) {
-        // hlavní smyčka prázdná
-        if (buttonPD2isPressed == 1) {
+        
+        if (buttonPD2isPressed == 1 || buttonPD3isPressed == 1) { 
           tim1_ovf_33ms();
           tim1_ovf_enable();
         } else{
           tim1_ovf_disable();
           tim1_stop();
-        };
+        }; // když je zmáčknuté tlačítko 1 nebo 2, tak se spouští timer (rozpoznání krátkého/dlouhého stisku)
           
     }
 }
 
 ISR(PCINT2_vect)
 {
+    
     uint8_t newD = PIND;   // čteme celý port D
 
-    // PD2 (PCINT18)
+    // PD2 (PCINT18) stisknutí - zvýší frekvenci o 100 (krátký i dlouhý stisk)
     if ((newD & (1 << PD2)) == 0 && (oldD & (1 << PD2)) != 0) {
         
+      /*
+      if (zkouska12 = 1) { // xxxxxxxxxxxxxxxxxxxxxx vvvvvvvvvvvvv
         gpio_toggle(&PORTB, PB5);
-        /*
+      } else { // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ^^^^^^^^^^^^^
         actFrequency = SI4703_GetFreq();
         actFrequency = actFrequency + 100;
         SI4703_SetFreq(actFrequency);
-        */
-       buttonPD2isPressed = 1;
+      } // xxxxxxxxxxxxxxxxxxxxxxxxx
+      */
+
+      if (buttonPD3isPressed != 1) {
+      buttonPD2isPressed = 1;
+      }
     }
 
+    // PD2 (PCINT18) uvolnění
     if ((newD & (1 << PD2)) != 0 && (oldD & (1 << PD2)) == 0) {
   
      buttonPD2isPressed = 0;
-     buttonPD2pressedLong = 0;
-     buttonPD2pressedLong2 = 0;
+     buttonPressedLong = 0;
+     buttonPressedLong2 = 0;
 
     }
 
-    // PD3 (PCINT19)
+    // PD3 (PCINT19) stisknutí - sníží frekvenci o 100 (krátký i dlouhý stisk)
     if ((newD & (1 << PD3)) == 0 && (oldD & (1 << PD3)) != 0) {
-        // sem dej akci pro tlačítko PD3
-        gpio_write_high(&PORTB, PB5);
+        
+      /*
+        if (zkouska12 = 1) { // xxxxxxxxxxxxxxxxxxxxxx vvvvvvvvvvvvv
+          gpio_toggle(&PORTB, PB5);
+        } else { // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ^^^^^^^^^^^^^
+          actFrequency = SI4703_GetFreq();
+          actFrequency = actFrequency - 100;
+          SI4703_SetFreq(actFrequency);
+        } // xxxxxxxxxxxxxxxxxxxxxxxxx
+      */
+
+      if (buttonPD2isPressed != 1) {
+        buttonPD3isPressed = 1;
+        }
     }
 
-    // PD4 (PCINT20)
+    // PD3 (PCINT18) uvolnění
+    if ((newD & (1 << PD3)) != 0 && (oldD & (1 << PD3)) == 0) {
+        
+        buttonPD3isPressed = 0;
+        buttonPressedLong = 0;
+        buttonPressedLong2 = 0;
+    }
+
+
+    // PD4 (PCINT20) - funkce seek - najde nejbližší stanici na vyšší frekvenci
     if ((newD & (1 << PD4)) == 0 && (oldD & (1 << PD4)) != 0) {
-        // sem dej akci pro tlačítko PD4
-        gpio_write_low(&PORTB, PB5);
+        
+        if (zkouska12 = 1) { // xxxxxxxxxxxxxxxxxxxxxx vvvvvvvvvvvvv
+          gpio_toggle(&PORTB, PB5);
+        } else { // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ^^^^^^^^^^^^^
+          SI4703_SeekUp();
+        } // xxxxxxxxxxxxxxxxxxxxxxxxx
+           
     }
   oldD = newD;
 }
@@ -102,55 +134,96 @@ volatile uint8_t fastTime = 0; // pocitadlo pro zvyseni prodlevy pred zrychlenim
 
 ISR(TIMER1_OVF_vect)
 {
-    
-    
+    if (zkouska12 != 1) { // xxxxxxxxxxxxxxxxxxxx
+      actFrequency = SI4703_GetFreq();
+    } // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-    if (buttonPD2pressedLong == 0 && buttonPD2pressedLong2 == 0) {
+    if (buttonPressedLong == 0 && buttonPressedLong2 == 0) { // krátký stisk - jdenorázová změna frekvence o 100
         if (initTime > 20) {
-          buttonPD2pressedLong = 1;
-          gpio_toggle(&PORTB, PB5);
+          buttonPressedLong = 1;
+
+          if (zkouska12 == 1) { // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx vvvvvvvvvvvv
+
+            gpio_toggle(&PORTB, PB5); // ===================================== AKCE 1 PRO PD2 A PD3 TLACITKA SPOLU (ZATIM - UP, DOWN)
+          
+          } else {
+
+            if (buttonPD2isPressed == 1) {
+              actFrequency += 100;
+            } else if (buttonPD3isPressed == 1) {
+              actFrequency -= 100;
+            } 
+
+            SI4703_SetFreq(actFrequency);
+          } // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ^^^^^^^^^^
+          
           initTime = 0;
         } else {
             initTime++;
         }
-    } else if (buttonPD2pressedLong == 1 && buttonPD2pressedLong2 == 0){
+
+    } else if (buttonPressedLong == 1 && buttonPressedLong2 == 0){ // dlouhý stisk - frekvence skáče v pravidelných intervalech nahoru/dolů
       
-        if (initTime > 10) {
-          gpio_toggle(&PORTB, PB5);
+        if (initTime > 6) {
+          
+          if (zkouska12 == 1) { // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx vvvvvvvvvvvv
+
+            gpio_toggle(&PORTB, PB5);
+          
+          } else {
+
+            if (buttonPD2isPressed == 1) {
+              actFrequency += 100;
+            } else if (buttonPD3isPressed == 1) {
+              actFrequency -= 100;
+            } 
+
+            SI4703_SetFreq(actFrequency);
+          } // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ^^^^^^^^^^
+
           initTime = 0;
+
           if (fastTime > 6) {
-              buttonPD2pressedLong2 = 1;
+              buttonPressedLong2 = 1;
               fastTime = 0;
           } else {
               fastTime++;
           }
+
         } else {
             initTime++;
         }
-    } else if (buttonPD2pressedLong == 1 && buttonPD2pressedLong2 == 1){
+
+    } else if (buttonPressedLong == 1 && buttonPressedLong2 == 1){ // nejdelší stisk - změna frekvence zrychlí
       
-        if (initTime > 4) {
-          gpio_toggle(&PORTB, PB5);
+        if (initTime > 2) {
+
+          if (zkouska12 == 1) { // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx vvvvvvvvvvvv
+
+            gpio_toggle(&PORTB, PB5);
+          
+          } else {
+
+            if (buttonPD2isPressed == 1) {
+              actFrequency += 100;
+            } else if (buttonPD3isPressed == 1) {
+              actFrequency -= 100;
+            } 
+
+            SI4703_SetFreq(actFrequency);
+          } // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ^^^^^^^^^^
+
           initTime = 0;
+          
         } else {
             initTime++;
         }
     } else {
-        buttonPD2pressedLong = 0;
-        buttonPD2pressedLong2 = 0;
+        buttonPressedLong = 0;
+        buttonPressedLong2 = 0;
         initTime = 0;
         fastTime = 0;
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
+// =============================================================================== KONEC SEKCE TLACITKA
